663601be02cd2855476c027130e5ac31ae5265db
Steven Suing <Steven.suing@gmail.com>
2013-10-05 13:46:41 -0400

    Added PubSub

---
+++ PubSubPublisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace PubSubPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = "LocalHost";
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel model = connection.CreateModel();
+            
+            //Declare exchange here
+           
+            
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            model.BasicPublish("", "TRINUG", basicProperties, System.Text.Encoding.UTF8.GetBytes("Hello TRINUG: "));
+            Console.WriteLine("Message Sent");
+
+            model.Close();
+            connection.Close();
+        }
+    }
+}
